2d8fa07a662d6fb5ed9abe2b351a637c9e0055e0
sandeep2255 <sandeeppradeep2255@gmail.com>
2023-03-13 04:09:05 +0530

    Created file upload input tag and the function onchange

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import XLSX from 'xlsx';
 
 function App() {
+  const [excelData, setExcelData] = useState([]);
+
+  const handleFileUpload = (e)=>{
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) =>{
+      const data = new Uint8Array(e.target.result);
+      const workbook = XLSX.read(data, {type:'array'});
+    };
+    reader.readAsArrayBuffer(file)
+  }
+  
   return (
     <div className="App">
       <h1>MIGRATION APPLICATION</h1>
+      <div className='UploadFile'>
+        <label htmlFor='excel-file'>Upload Excel File:</label>
+        <input type="file" id='excel-file' name='excel-file' accept='.xlsx, .xls' onChange={handleFileUpload}/>
+      </div>
     </div>
   );
 }
