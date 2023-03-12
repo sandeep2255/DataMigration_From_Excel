4d910e4d40953510fa1f6a9268d934c6347d6ec2
sandeep2255 <sandeeppradeep2255@gmail.com>
2023-03-12 10:59:02 +0530

    Adding cors and axios to the project and connecting to db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3001;
+const mysql = require('mysql2');
+
+
+const db = mysql.createPool({
+    host:'localhost:3307',
+    user:'root',
+    password:'Sandeep@1999',
+    database:'RIET',
+});
 
 app.get('/', (req, res)=>{
     res.send("App running, Hello");
